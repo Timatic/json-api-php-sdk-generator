4e3d0226e688fcd13c310afc3834cfdace051ca4
Tomas van Rijsse <tomas@pionect.nl>
2026-01-22 09:44:02 +0100

    run pint silently

--- src/Services/PintRunner.php
+++ src/Services/PintRunner.php
@@ -10,13 +10,10 @@ class PintRunner
 {
     public function run(string $outputDir, SymfonyStyle $io): void
     {
-        $io->section('Formatting Code');
-
         $pintPath = "{$outputDir}/vendor/bin/pint";
 
-        if (! file_exists($pintPath)) {
+        if (!file_exists($pintPath)) {
             $io->warning('Pint not found. Skipping code formatting. Run "composer require --dev laravel/pint" to enable.');
-
             return;
         }
 
@@ -27,23 +24,18 @@ class PintRunner
             2 => ['pipe', 'w'],
         ];
 
-        $process = proc_open("{$pintPath} {$outputDir}/src {$outputDir}/tests {$outputDir}/config {$outputDir}/factories", $descriptorSpec, $pipes, $outputDir);
+        $process = proc_open("{$pintPath} {$outputDir}/src {$outputDir}/tests {$outputDir}/config {$outputDir}/factories -q", $descriptorSpec, $pipes, $outputDir);
 
-        if (! \is_resource($process)) {
+        if (!is_resource($process)) {
             $io->warning('Failed to start Pint process.');
-
             return;
         }
 
-        $stdout = stream_get_contents($pipes[1]);
+        stream_get_contents($pipes[1]);
         $stderr = stream_get_contents($pipes[2]);
         fclose($pipes[1]);
         fclose($pipes[2]);
         $exitCode = proc_close($process);
-
-        if ($stdout) {
-            $io->write($stdout);
-        }
 
         if ($exitCode !== 0) {
             $io->warning("Pint formatting failed ({$exitCode}).\n{$stderr}");
